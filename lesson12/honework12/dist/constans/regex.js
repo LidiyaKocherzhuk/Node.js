2fbccde08f8964e6acce97392745566f40c6cd6d
LidiyaKocherzhuk <91597998+LidiyaKocherzhuk@users.noreply.github.com>
2022-04-04 22:05:39 +0300

    dist delete

--- lesson12/honework12/dist/constans/regex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.regexp = void 0;
-exports.regexp = {
-    EMAIL: /^(\w+\.)*\w+@\w+\.[a-z]{2,6}$/,
-    PHONE: /^\+\d{2}\(\d{3}\)\d{3}-\d{2}-\d{2}$/,
-    PASSWORD: /(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]{8,}/,
-};
-//# sourceMappingURL=regex.js.map
