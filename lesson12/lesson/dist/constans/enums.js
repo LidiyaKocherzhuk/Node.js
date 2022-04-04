05fc1b4fd12b863635de648ed81ace894d1a3d24
LidiyaKocherzhuk <91597998+LidiyaKocherzhuk@users.noreply.github.com>
2022-04-04 22:05:14 +0300

    dist delete

--- lesson12/lesson/dist/constans/enums.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.EmailActionEnum = void 0;
-var EmailActionEnum;
-(function (EmailActionEnum) {
-    EmailActionEnum[EmailActionEnum["WELCOME"] = 0] = "WELCOME";
-    EmailActionEnum[EmailActionEnum["ACCOUNT_BLOCKED"] = 1] = "ACCOUNT_BLOCKED";
-})(EmailActionEnum = exports.EmailActionEnum || (exports.EmailActionEnum = {}));
-//# sourceMappingURL=enums.js.map
